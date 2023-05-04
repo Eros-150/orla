52481cef6893accca50b50ca29c1d4ea2baf5dc3
Sergi <78415500+sergipermar@users.noreply.github.com>
2023-05-04 19:15:02 +0200

    a

--- client/src/components/body.js
+++ client/src/components/body.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const body = () => {
-    
+const body = ({image, title, description}) => {
+    return (
+        <div className='card'>
+            <img src={"../src/assets/logo.png"} alt={title} />
+            <div className='card-body'>
+                <h5 className='card-title'>Nombre del alumno{title}</h5>
+                <p className='card-text'>Descripción{description}</p>
+            </div>
+
+            <img src={"../src/assets/logo.png"} alt={title} />
+            <div className='card-body'>
+                <h5 className='card-title'>Nombre del alumno{title}</h5>
+                <p className='card-text'>Descripción{description}</p>
+            </div>
+
+            <img src={"../src/assets/logo.png"} alt={title} />
+            <div className='card-body'>
+                <h5 className='card-title'>Nombre del alumno{title}</h5>
+                <p className='card-text'>Descripción{description}</p>
+            </div>
+        </div>   
+    )
 }
 
 export default body;
