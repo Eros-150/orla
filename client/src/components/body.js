3c18762b66cfe1a334f129536db70517c540834f
Sergi <78415500+sergipermar@users.noreply.github.com>
2023-05-04 19:52:25 +0200

    a

--- client/src/components/body.js
+++ client/src/components/body.js
@@ -1,26 +1,61 @@
 import React from 'react';
 
-const body = ({image, title, description}) => {
+const body = ({ image, title, description }) => {
     return (
         <div className='card'>
             <img src={"../src/assets/logo.png"} alt={title} />
             <div className='card-body'>
-                <h5 className='card-title'>Nombre del alumno{title}</h5>
+                <h5 className='card-title'>Nombre del alumno1{title}</h5>
                 <p className='card-text'>Descripción{description}</p>
             </div>
 
             <img src={"../src/assets/logo.png"} alt={title} />
             <div className='card-body'>
-                <h5 className='card-title'>Nombre del alumno{title}</h5>
+                <h5 className='card-title'>Nombre del alumno2{title}</h5>
                 <p className='card-text'>Descripción{description}</p>
             </div>
 
             <img src={"../src/assets/logo.png"} alt={title} />
             <div className='card-body'>
-                <h5 className='card-title'>Nombre del alumno{title}</h5>
+                <h5 className='card-title'>Nombre del alumno3{title}</h5>
                 <p className='card-text'>Descripción{description}</p>
             </div>
-        </div>   
+
+            <img src={"../src/assets/logo.png"} alt={title} />
+            <div className='card-body'>
+                <h5 className='card-title'>Nombre del alumno4{title}</h5>
+                <p className='card-text'>Descripción{description}</p>
+            </div>
+
+            <img src={"../src/assets/logo.png"} alt={title} />
+            <div className='card-body'>
+                <h5 className='card-title'>Nombre del alumno5{title}</h5>
+                <p className='card-text'>Descripción{description}</p>
+            </div>
+
+            <img src={"../src/assets/logo.png"} alt={title} />
+            <div className='card-body'>
+                <h5 className='card-title'>Nombre del alumno6{title}</h5>
+                <p className='card-text'>Descripción{description}</p>
+            </div>
+
+            <div className='info'>
+                <table>
+                    <tr>
+                        <td><strong>Nombre:</strong></td>
+                        <td>John Doe</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Edad:</strong></td>
+                        <td>25</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Correo electrónico:</strong></td>
+                        <td>john.doe@example.com</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     )
 }
 
