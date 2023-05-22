ce4d77fc27aee60e385d02cc558b2a8cc945ac90
Sergi <78415500+sergipermar@users.noreply.github.com>
2023-05-22 15:51:40 +0200

    a

---
+++ client/src/components/quienSomos.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './about-us.css';
+import fut1 from './asssets/fut2.jpg';
+import fut2 from './asssets/fut3.jpg';
+import fotoeros from '../img/fotomarc.jpg';
+import fotojoan from '../img/Joan_diaz.jpg';
+import fotozeji from '../img/sergi_pérez.jpg'
+import { Row, Col } from 'react-bootstrap';
+
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <div className="form2">
+                <h2 className="titulo2" style={{fontSize:"70px", marginBottom: "5vh"}}>Abous Us</h2>
+        <p style={{ fontSize: '25px' }}>Somos unos estudiantes del Grado Superior de Desarrollo de Aplicaciones Web y estamos presentando nuestra idea en formato web para el trabajo final de curso.</p><br></br>
+        <p style={{ fontSize: '25px' }}>Desde el inicio del proyecto, hemos trabajado para mejorar lo máximo posible la visión e idea de nuestro proyecto.</p><br></br>
+        <p style={{ fontSize: '25px' }}>Nuestro equipo está compuesto por expertos en el desarrollo de aplicaciones web.<br></br> Trabajamos con pasión y compromiso para ofrecer un proyecto de calidad.</p>
+        <Row className='center-row'>
+          <Col md={10} sm={4} xs={4}>
+            <Row>
+              <Col md={3} sm={3} xs={3}>
+                <div><img className='fotos2' src={fotoeros} /><h3>Eros Ruíz</h3></div>
+              </Col>
+              <Col md={3} sm={3} xs={3}>
+                <div><img className='fotos2' src={fotojoan} /><h3>Joan Díaz</h3></div>
+              </Col>
+              <Col md={3} sm={3} xs={3}>
+                <div><img className='fotos2' src={fotozeji} /><h3>Sergi Pérez</h3></div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+
+        {/* <marquee>CREADORES: MARC CLAPÉS, ALEIX MARTÍN, JOAN ANGEL DÍAZ Y SERGI PÉREZ</marquee> */}
+      </div>
+    )
+  }
+}
