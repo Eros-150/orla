8f59406b27c32d2b19e1c15a72e7913e9974e903
joandiaper <joandiaper@campus.monlau.com>
2023-05-24 19:05:33 +0200

    aa

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -8,15 +8,15 @@ const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <footer className="footer mt-5">
+    <footer className="footer mt-5 bg-dark text-white">
       <div className="container">
         <div className="row">
           <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
             <p className='p'>{year} 2DAW Monlau &copy; All Rights Reserved.</p>
             <nav className="footer-nav">
               <ul>
-                <li><a href="/about">About Us</a></li>
-                <li><a href="/contact">Contact Us</a></li>
+                <li><a href="/about" className="text-white">About Us</a></li>
+                <li><a href="/contact" className="text-white">Contact Us</a></li>
               </ul>
             </nav>
           </div>
