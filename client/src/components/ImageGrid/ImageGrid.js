dc6701853423829b355cec295b23c36be7bd762b
joandiaper <joandiaper@campus.monlau.com>
2023-05-24 16:13:18 +0200

    aa

--- client/src/components/ImageGrid/ImageGrid.js
+++ client/src/components/ImageGrid/ImageGrid.js
@@ -11,12 +11,14 @@ const ImageGrid = () => {
             className="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-4"
           >
             <div className="card h-75">
+              <a href={image.web} target="_blank">
               <img src={image.imageUrl} className="card-img-top" />
+              </a>
               <div className="card-body"></div>
             </div>
           </div>
         ))}
-        ;
+        
       </div>
     </div>
   );
